1f0ba3c5d9cace156f0016837a14a02b25343b5c
ckyrouac <ckyrouac@redhat.com>
2015-07-23 13:40:04 -0400

    Refactor PropertiesHandler

--- src/main/java/com/redhat/telemetry/integration/sat5/util/PropertiesHandler.java
+++ src/main/java/com/redhat/telemetry/integration/sat5/util/PropertiesHandler.java
@@ -1,7 +1,6 @@
 package com.redhat.telemetry.integration.sat5.util;
 
 import java.io.File;
-import java.util.NoSuchElementException;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -12,57 +11,69 @@ public class PropertiesHandler {
   private static Logger LOG = LoggerFactory.getLogger(PropertiesHandler.class);
 
   public static String getRHEL6ChannelLabel() throws ConfigurationException {
-    String label = "rhel-x86_64-server-6";
-    try {
-      label = (String) getProperty(Constants.RHEL6_CHANNEL_LABEL_PROPERTY, Constants.STRING_TYPE);
-    } catch (NoSuchElementException e) {
-      setProperty(Constants.RHEL6_CHANNEL_LABEL_PROPERTY, label);
-    }
-    return label;
+    String rhel6ChannelLabel = (String) getProperty(
+        Constants.RHEL6_CHANNEL_LABEL_PROPERTY,
+        Constants.STRING_TYPE,
+        "rhel-x86_64-server-6");
+    return rhel6ChannelLabel;
   }
 
   public static String getRHEL7ChannelLabel() throws ConfigurationException {
-    String label = "rhel-x86_64-server-7.1.z";
-    try {
-      label = (String) getProperty(Constants.RHEL7_CHANNEL_LABEL_PROPERTY, Constants.STRING_TYPE);
-    } catch (NoSuchElementException e) {
-      LOG.debug("rhel7channellabel property missing, setting it to default");
-      setProperty(Constants.RHEL7_CHANNEL_LABEL_PROPERTY, label);
-    }
-    return label;
+    String rhel7ChannelLabel = (String) getProperty(
+        Constants.RHEL7_CHANNEL_LABEL_PROPERTY,
+        Constants.STRING_TYPE,
+        "rhel-x86_64-server-7");
+    return rhel7ChannelLabel;
   }
 
   public static String getRPMName() throws ConfigurationException {
-    String rpmName = "redhat-access-insights";
-    try {
-      rpmName = (String) getProperty(Constants.RPM_NAME_PROPERTY, Constants.STRING_TYPE);
-    } catch (NoSuchElementException e) {
-      LOG.debug("rpmname property missing, setting it to default");
-      setProperty(Constants.RPM_NAME_PROPERTY, rpmName);
-    }
+    String rpmName = (String) getProperty(
+        Constants.RPM_NAME_PROPERTY,
+        Constants.STRING_TYPE,
+        "redhat-access-insights");
     return rpmName;
   }
 
   public static boolean getEnabled() throws ConfigurationException {
-    boolean enabled = false;
-    try {
-      enabled = (Boolean) getProperty(Constants.ENABLED_PROPERTY, Constants.BOOLEAN_TYPE);
-    } catch (NoSuchElementException e) {
-      LOG.debug("enabled property missing, setting it to default");
-      setProperty(Constants.ENABLED_PROPERTY, Boolean.toString(enabled));
-    }
+    boolean enabled = (Boolean) getProperty(
+        Constants.ENABLED_PROPERTY,
+        Constants.BOOLEAN_TYPE,
+        Boolean.toString(false));
     return enabled;
   }
 
   public static boolean getDebug() throws ConfigurationException {
-    boolean debug = false;
-    try {
-      debug = (Boolean) getProperty(Constants.DEBUG_PROPERTY, Constants.BOOLEAN_TYPE);
-    } catch (NoSuchElementException e) {
-      LOG.debug("debug property missing, setting it to default");
-      setProperty(Constants.DEBUG_PROPERTY, Boolean.toString(debug));
-    }
+    boolean debug = (Boolean) getProperty(
+        Constants.DEBUG_PROPERTY, 
+        Constants.BOOLEAN_TYPE, 
+        Boolean.toString(false));
     return debug;
+  }
+
+  private static Object getProperty(
+      String propertyName, 
+      String propertyType,
+      String defaultValue) throws ConfigurationException {
+
+    Object property = defaultValue;
+    try {
+      property = getProperty(propertyName, propertyType);
+      if (property == null) {
+        LOG.info(
+            propertyName + 
+            " property is missing. Setting it to the default value: " + 
+            defaultValue);
+        property = defaultValue;
+      }
+      LOG.debug(propertyName + " property: " + property);
+    } catch (Exception e) {
+      LOG.info(
+          propertyName + 
+          " property is missing. Setting it to the default value: " + 
+          defaultValue);
+      setProperty(propertyName, defaultValue);
+    }
+    return property;
   }
 
   public static String getPortalUrl() throws ConfigurationException {
