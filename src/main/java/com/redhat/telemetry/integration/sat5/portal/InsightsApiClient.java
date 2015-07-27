b15a1ec734c9934b674d3e4cb14152e14db920b9
ckyrouac <ckyrouac@redhat.com>
2015-07-27 16:07:32 -0400

    Refactor InsightsApiClient

--- src/main/java/com/redhat/telemetry/integration/sat5/portal/InsightsApiClient.java
+++ src/main/java/com/redhat/telemetry/integration/sat5/portal/InsightsApiClient.java
@@ -1,58 +1,42 @@
 package com.redhat.telemetry.integration.sat5.portal;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.security.cert.CertificateException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import javax.net.ssl.SSLContext;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.WebApplicationException;
 
 import org.apache.commons.configuration.ConfigurationException;
-import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
-import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.redhat.telemetry.integration.sat5.json.PortalResponse;
 import com.redhat.telemetry.integration.sat5.util.Constants;
-import com.redhat.telemetry.integration.sat5.util.PropertiesHandler;
+import com.redhat.telemetry.integration.sat5.util.Util;
 
 public class InsightsApiClient {
   private Logger LOG = LoggerFactory.getLogger(InsightsApiClient.class);
   private CloseableHttpClient client = null;
-  private HttpClientContext context = HttpClientContext.create();
   private String portalUrl = Constants.PORTAL_URL;
-  private RequestConfig requestConfig = null;
 
   public InsightsApiClient() 
       throws NoSuchAlgorithmException, 
@@ -62,9 +46,9 @@ public class InsightsApiClient {
              KeyManagementException,
              ConfigurationException {
     
-    loadPortalUrl();
+    this.portalUrl = Util.loadPortalUrl();
     client = HttpClients.custom()
-            .setSSLSocketFactory(loadSSLKeystore())
+            .setSSLSocketFactory(Util.loadSSLKeystore())
             .build();
   }
 
@@ -107,12 +91,12 @@ public class InsightsApiClient {
       throw new WebApplicationException(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
     }
 
-    loadProxyInfo();
-    request.setConfig(requestConfig);
+    RequestConfig proxyInfo = Util.loadProxyInfo();
+    request.setConfig(proxyInfo);
     LOG.debug("Accept header: " + responseContentType);
     request.addHeader(HttpHeaders.ACCEPT, responseContentType);
     request.addHeader(Constants.SYSTEMID_HEADER, getSatelliteSystemId());
-    HttpResponse response = client.execute(request, context);
+    HttpResponse response = client.execute(request, Util.loadProxyCreds());
     HttpEntity responseEntity = response.getEntity();
     String stringEntity = "";
     if (responseEntity != null) {
@@ -143,81 +127,5 @@ public class InsightsApiClient {
   }
 
 
-  /**
-   * Load rhai.keystore
-   */
-  private SSLConnectionSocketFactory loadSSLKeystore() 
-      throws NoSuchAlgorithmException, 
-             KeyStoreException, 
-             CertificateException, 
-             IOException, 
-             KeyManagementException {
-    LOG.debug("Loading rhai.keystore");
-    SSLContext sslcontext = SSLContexts.custom()
-            .loadTrustMaterial(
-                new File(Constants.SSL_KEY_STORE_LOC), 
-                Constants.SSL_KEY_STORE_PW.toCharArray(),
-                new TrustSelfSignedStrategy())
-            .build();
-    SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-            sslcontext,
-            new String[] { "TLSv1" },
-            null,
-            SSLConnectionSocketFactory.getDefaultHostnameVerifier());
-    return sslsf;
-  }
-
-  /**
-   * Set this.portalUrl to value from properties file
-   */
-  private void loadPortalUrl() throws ConfigurationException {
-    String configPortalUrl = PropertiesHandler.getPortalUrl();
-    if (configPortalUrl != null) {
-      if (configPortalUrl.charAt(configPortalUrl.length() - 1) != '/') {
-        configPortalUrl = configPortalUrl + "/";
-      }
-      this.portalUrl = configPortalUrl;
-    }
-  }
-
-  /**
-   * Add proxy info to this.requestConfig if proxy is defined in rhn.conf
-   */
-  private void loadProxyInfo() throws ConfigurationException {
-    PropertiesConfiguration properties = new PropertiesConfiguration();
-    properties.load(Constants.RHN_CONF_LOC); 
-    String proxyHostColonPort = properties.getString(Constants.RHN_CONF_HTTP_PROXY);
-    if (proxyHostColonPort != null && !proxyHostColonPort.equals(null) && !proxyHostColonPort.equals("")) {
-      //pull out the port from the http_proxy property
-      int proxyPort = 80;
-      String hostname = "";
-      if (proxyHostColonPort.contains(":")) {
-        Pattern portPattern = Pattern.compile("(.*):([0-9]*)$");
-        Matcher portMatcher = portPattern.matcher(proxyHostColonPort);
-        if (portMatcher.matches()) {
-          hostname = portMatcher.group(1);
-          proxyPort = Integer.parseInt(portMatcher.group(2));
-        }
-      } else {
-        hostname = proxyHostColonPort;
-      }
-
-      //set the username/password for the proxy
-      String proxyUser = properties.getString(Constants.RHN_CONF_HTTP_PROXY_USERNAME);
-      String proxyPassword = properties.getString(Constants.RHN_CONF_HTTP_PROXY_PASSWORD);
-      if (proxyUser != null && proxyUser != "" && proxyPassword != null && proxyPassword != "") {
-        CredentialsProvider credsProvider = new BasicCredentialsProvider();
-        credsProvider.setCredentials(
-            new AuthScope(hostname, proxyPort),
-            new UsernamePasswordCredentials(proxyUser, proxyPassword));
-        context.setCredentialsProvider(credsProvider);
-        LOG.debug("Proxyuser: " + proxyUser);
-      }
-
-      LOG.debug("Satellite is configured to use a proxy. Host: " + hostname + " | Port: " + Integer.toString(proxyPort));
-      HttpHost proxy = new HttpHost(hostname, proxyPort);
-      this.requestConfig = RequestConfig.custom().setProxy(proxy).build();
-    }
-  }
 }
 
--- src/main/java/com/redhat/telemetry/integration/sat5/util/Util.java
+++ src/main/java/com/redhat/telemetry/integration/sat5/util/Util.java
@@ -1,15 +1,36 @@
 package com.redhat.telemetry.integration.sat5.util;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.net.ssl.SSLContext;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.ssl.SSLContexts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,8 +39,9 @@ import com.redhat.telemetry.integration.sat5.satellite.SatApi;
 import ch.qos.logback.classic.Level;
 
 public class Util {
+  private static Logger LOG = LoggerFactory.getLogger(Util.class);
+
   public static String getSatelliteHostname() throws IOException, InterruptedException {
-    Logger LOG = LoggerFactory.getLogger(Util.class);
     String hostname = "";
     try {
       hostname = InetAddress.getLocalHost().getHostName();
@@ -62,8 +84,6 @@ public class Util {
 
   public static String getLog(Date currentDate) throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader("/var/log/rhn/rhai.log"));
-    //SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS");
-    //String currentTimestamp = dateFormat.format(calendar.getTime());
     try {
       StringBuilder builder = new StringBuilder();
       String line = reader.readLine();
@@ -117,5 +137,118 @@ public class Util {
       }
     }
     return response;
+  }
+
+  /**
+   * Retrieve the portal_url property from disk
+   */
+  public static String loadPortalUrl() throws ConfigurationException {
+    String response = Constants.PORTAL_URL;
+    String configPortalUrl = PropertiesHandler.getPortalUrl();
+    if (configPortalUrl != null) {
+      if (configPortalUrl.charAt(configPortalUrl.length() - 1) != '/') {
+        configPortalUrl = configPortalUrl + "/";
+      }
+      response = configPortalUrl;
+    }
+    return response;
+  }
+
+  /**
+   * Add proxy info to this.requestConfig if proxy is defined in rhn.conf
+   */
+  public static RequestConfig loadProxyInfo() throws ConfigurationException {
+    RequestConfig proxyInfo = null;
+    String hostname = getProxyHostname();
+    int port = getProxyPort();
+    if (hostname != null && !hostname.equals("")) {
+      LOG.debug("Satellite is configured to use a proxy. Host: " + 
+          hostname + " | Port: " + Integer.toString(port));
+      HttpHost proxy = new HttpHost(hostname, port);
+      proxyInfo = RequestConfig.custom().setProxy(proxy).build();
+    }
+    return proxyInfo;
+  }
+
+  /**
+   * Retrieve hostname as http_proxy value from rhn.conf
+   */
+  public static String getProxyHostname() throws ConfigurationException {
+    PropertiesConfiguration properties = new PropertiesConfiguration();
+    properties.load(Constants.RHN_CONF_LOC); 
+    String proxyHostColonPort = properties.getString(Constants.RHN_CONF_HTTP_PROXY);
+    String hostname = "";
+    if (proxyHostColonPort.contains(":")) {
+      Pattern portPattern = Pattern.compile("(.*):([0-9]*)$");
+      Matcher portMatcher = portPattern.matcher(proxyHostColonPort);
+      if (portMatcher.matches()) {
+        hostname = portMatcher.group(1);
+      }
+    } else {
+      hostname = proxyHostColonPort;
+    }
+    return hostname; 
+  }
+
+  /**
+   * Retrieve port from http_proxy value in rhn.conf
+   */
+  public static int getProxyPort() throws ConfigurationException {
+    PropertiesConfiguration properties = new PropertiesConfiguration();
+    properties.load(Constants.RHN_CONF_LOC); 
+    String proxyHostColonPort = properties.getString(Constants.RHN_CONF_HTTP_PROXY);
+    int port = 80;
+    if (proxyHostColonPort.contains(":")) {
+      Pattern portPattern = Pattern.compile("(.*):([0-9]*)$");
+      Matcher portMatcher = portPattern.matcher(proxyHostColonPort);
+      if (portMatcher.matches()) {
+        port = Integer.parseInt(portMatcher.group(2));
+      }
+    } 
+    return port;
+  }
+
+  /**
+   * Retrieve creds as HttpClientContext from rhn.conf
+   */
+  public static HttpClientContext loadProxyCreds() throws ConfigurationException {
+    HttpClientContext context = HttpClientContext.create();
+    PropertiesConfiguration properties = new PropertiesConfiguration();
+    properties.load(Constants.RHN_CONF_LOC); 
+    String proxyUser = properties.getString(Constants.RHN_CONF_HTTP_PROXY_USERNAME);
+    String proxyPassword = properties.getString(Constants.RHN_CONF_HTTP_PROXY_PASSWORD);
+    if (proxyUser != null && proxyUser != "" && proxyPassword != null && proxyPassword != "") {
+      CredentialsProvider credsProvider = new BasicCredentialsProvider();
+      credsProvider.setCredentials(
+          new AuthScope(getProxyHostname(), getProxyPort()),
+          new UsernamePasswordCredentials(proxyUser, proxyPassword));
+      context.setCredentialsProvider(credsProvider);
+      LOG.debug("Proxyuser: " + proxyUser);
+    }
+    return context;
+  }
+
+  /**
+   * Load rhai.keystore
+   */
+  public static SSLConnectionSocketFactory loadSSLKeystore() 
+      throws NoSuchAlgorithmException, 
+             KeyStoreException, 
+             CertificateException, 
+             IOException, 
+             KeyManagementException {
+    LOG.debug("Loading rhai.keystore");
+    SSLContext sslcontext = SSLContexts.custom()
+            .loadTrustMaterial(
+                new File(Constants.SSL_KEY_STORE_LOC), 
+                Constants.SSL_KEY_STORE_PW.toCharArray(),
+                new TrustSelfSignedStrategy())
+            .build();
+    SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+            sslcontext,
+            new String[] { "TLSv1" },
+            null,
+            SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+    return sslsf;
   }
 }
